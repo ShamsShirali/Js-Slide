b099934b505bed6d115b490a21d0c951740562b1
unknown <shamsshsh@code.edu.az>
2023-04-09 19:04:05 +0400

    js slide

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,32 @@
 "use strict";
 
+let last=document.querySelector('.last');
+let next=document.querySelector('.next');
+
+last.addEventListener('click',()=>{
+    let activeImage=document.querySelector('.active');
+
+    if(activeImage.previousElementSibling!=null){
+     activeImage.classList.remove('active');
+     activeImage.previousElementSibling.classList.add('active');
+    }
+    else{
+     activeImage.classList.remove('active');
+     activeImage.parentNode.lastElementChild.classList.add('active');
+    }
+
+    console.log('hello');
+});
+
+next.addEventListener('click',()=>{
+   let activeImage=document.querySelector('.active');
+
+   if(activeImage.nextElementSibling!=null){
+    activeImage.classList.remove('active');
+    activeImage.nextElementSibling.classList.add('active');
+   }
+   else{
+    activeImage.classList.remove('active');
+    activeImage.parentNode.firstElementChild.classList.add('active');
+   }
+});
